ae862afc424974d26328d569a523d7145b34e2fb
kapilkd13 <kapilkd13@gmail.com>
2017-08-25 15:36:44 +0530

    adding test suite to the  cwltest module

---
+++ tests/test_argparse.py
@@ -0,0 +1,18 @@
+import unittest
+from cwltest import arg_parser
+
+class TestArgparse(unittest.TestCase):
+    def setUp(self):
+        self.parser = arg_parser()
+
+    def test_arg(self):
+        parsed = self.parser.parse_args(['--test', 'test_name','-n','52','--tool','cwltool','-j','4'])
+        self.assertEqual(parsed.test, 'test_name')
+        self.assertEqual(parsed.n, '52')
+        self.assertEqual(parsed.tool, 'cwltool')
+        self.assertEqual(parsed.j, 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
