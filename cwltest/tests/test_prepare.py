6226d055243c25ac7cc51076e32b4bde0c371560
Michael R. Crusoe <1330696+mr-c@users.noreply.github.com>
2018-05-12 20:08:08 +0300

    enable appveyor testing of cwltest (#72)
    * enable appveyor testing of cwltest
    * decrease default timeout
    * include mock-cwl-runner in appveyor
    * always upload the test results
    * enable pip cache preservation
    * make tests windows compatible
    * add system python to path
    * add test for path normalization
    * Fix the bug proven by the new test
    * speed up appveyor builds by removing pip, virtualenv installs & drop py-32bit versions

---
+++ cwltest/tests/test_prepare.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+from cwltest import prepare_test_command
+
+
+class TestPrepareCommand(unittest.TestCase):
+    """ Test prepare_test_command() """
+
+    def test_unix_relative_path(self):
+        """ Confirm unix style to windows style path corrections. """
+        command = prepare_test_command(
+            tool='cwl-runner',
+            args=[],
+            testargs=None,
+            test={'doc': 'General test of command line generation',
+                  'output': {'args': ['echo']},
+                  'tool': 'v1.0/bwa-mem-tool.cwl',
+                  'job': 'v1.0/bwa-mem-job.json',
+                  'tags': ['required']})
+        if os.name == 'nt':
+            self.assertEqual(command[3], 'v1.0\\bwa-mem-tool.cwl')
+            self.assertEqual(command[4], 'v1.0\\bwa-mem-job.json')
+        else:
+            self.assertEqual(command[3], 'v1.0/bwa-mem-tool.cwl')
+            self.assertEqual(command[4], 'v1.0/bwa-mem-job.json')
