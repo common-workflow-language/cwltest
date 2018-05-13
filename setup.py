14a3d00105a623e25d640b0dff007456ee7e45a1
requires.io <support@requires.io>
2018-05-13 20:16:40 +0300

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(name='cwltest',
       cmdclass={'egg_info': tagger},
       python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
       extras_require={':python_version<"3"': [
-                      'futures >= 3.0.5', 'subprocess32==3.5.0rc3'],
+                      'futures >= 3.0.5', 'subprocess32==3.5.0'],
                       ':python_version<"3.5"': ['typing >= 3.5.2'] }
 )
