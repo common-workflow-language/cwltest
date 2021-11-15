f2c7b003ddd2a904de0a0e4a5f346d2543d1c051
Michael R. Crusoe <michael.crusoe@gmail.com>
2021-11-15 18:29:02 +0100

    modernize tests

--- cwltest/tests/test_compare.py
+++ cwltest/tests/test_compare.py
@@ -1,64 +1,63 @@
-import unittest
 from .util import get_data
 from cwltest import CompareFail
 from cwltest.utils import compare_file, compare
 
+import pytest
 
-class TestCompareFile(unittest.TestCase):
-    def test_compare_file(self):
-        expected = {
-            "location": "cores.txt",
-            "size": 2,
-            "class": "File",
-            "checksum": "sha1$7448d8798a4380162d4b56f9b452e2f6f9e24e7a",
-        }
 
-        actual = {
-            "basename": "cores.txt",
-            "checksum": "sha1$7448d8798a4380162d4b56f9b452e2f6f9e24e7a",
-            "class": "File",
-            "location": "file:///var/folders/8x/2df05_7j20j6r8y81w4qf43r0000gn/T/tmpG0EkrS/cores.txt",
-            "path": "/var/folders/8x/2df05_7j20j6r8y81w4qf43r0000gn/T/tmpG0EkrS/cores.txt",
-            "size": 2,
-        }
-        try:
-            compare_file(expected, actual)
-        except CompareFail:
-            self.fail("File comparison failed unexpectedly")
+def test_compare_file():
+    expected = {
+        "location": "cores.txt",
+        "size": 2,
+        "class": "File",
+        "checksum": "sha1$7448d8798a4380162d4b56f9b452e2f6f9e24e7a",
+    }
 
-    def test_compare_contents_success(self):
-        expected = {
-            "location": "cores.txt",
-            "size": 2,
-            "class": "File",
-            "contents": "2\n",
-        }
+    actual = {
+        "basename": "cores.txt",
+        "checksum": "sha1$7448d8798a4380162d4b56f9b452e2f6f9e24e7a",
+        "class": "File",
+        "location": "file:///var/folders/8x/2df05_7j20j6r8y81w4qf43r0000gn/T/tmpG0EkrS/cores.txt",
+        "path": "/var/folders/8x/2df05_7j20j6r8y81w4qf43r0000gn/T/tmpG0EkrS/cores.txt",
+        "size": 2,
+    }
+    compare_file(expected, actual)
 
-        actual = {
-            "basename": "cores.txt",
-            "checksum": "sha1$7448d8798a4380162d4b56f9b452e2f6f9e24e7a",
-            "class": "File",
-            "location": "file:///var/folders/8x/2df05_7j20j6r8y81w4qf43r0000gn/T/tmpG0EkrS/cores.txt",
-            "path": get_data("tests/test-data/cores.txt"),
-            "size": 2,
-        }
-        compare(expected, actual)
 
-    def test_compare_contents_failure(self):
-        expected = {
-            "location": "cores.txt",
-            "size": 2,
-            "class": "File",
-            "contents": "2",
-        }
+def test_compare_contents_success():
+    expected = {
+        "location": "cores.txt",
+        "size": 2,
+        "class": "File",
+        "contents": "2\n",
+    }
 
-        actual = {
-            "basename": "cores.txt",
-            "checksum": "sha1$7448d8798a4380162d4b56f9b452e2f6f9e24e7a",
-            "class": "File",
-            "location": "file:///var/folders/8x/2df05_7j20j6r8y81w4qf43r0000gn/T/tmpG0EkrS/cores.txt",
-            "path": get_data("tests/test-data/cores.txt"),
-            "size": 2,
-        }
-        with self.assertRaises(CompareFail):
-            compare_file(expected, actual)
+    actual = {
+        "basename": "cores.txt",
+        "checksum": "sha1$7448d8798a4380162d4b56f9b452e2f6f9e24e7a",
+        "class": "File",
+        "location": "file:///var/folders/8x/2df05_7j20j6r8y81w4qf43r0000gn/T/tmpG0EkrS/cores.txt",
+        "path": get_data("tests/test-data/cores.txt"),
+        "size": 2,
+    }
+    compare(expected, actual)
+
+
+def test_compare_contents_failure():
+    expected = {
+        "location": "cores.txt",
+        "size": 2,
+        "class": "File",
+        "contents": "2",
+    }
+
+    actual = {
+        "basename": "cores.txt",
+        "checksum": "sha1$7448d8798a4380162d4b56f9b452e2f6f9e24e7a",
+        "class": "File",
+        "location": "file:///var/folders/8x/2df05_7j20j6r8y81w4qf43r0000gn/T/tmpG0EkrS/cores.txt",
+        "path": get_data("tests/test-data/cores.txt"),
+        "size": 2,
+    }
+    with pytest.raises(CompareFail):
+        compare_file(expected, actual)
--- cwltest/tests/test_prepare.py
+++ cwltest/tests/test_prepare.py
@@ -1,29 +1,26 @@
+"""Test prepare_test_command()"""
 import os
-import unittest
 from cwltest import prepare_test_command
 
 
-class TestPrepareCommand(unittest.TestCase):
-    """Test prepare_test_command()"""
-
-    def test_unix_relative_path(self):
-        """Confirm unix style to windows style path corrections."""
-        command = prepare_test_command(
-            tool="cwl-runner",
-            args=[],
-            testargs=None,
-            test={
-                "doc": "General test of command line generation",
-                "output": {"args": ["echo"]},
-                "tool": "v1.0/bwa-mem-tool.cwl",
-                "job": "v1.0/bwa-mem-job.json",
-                "tags": ["required"],
-            },
-            cwd=os.getcwd(),
-        )
-        if os.name == "nt":
-            self.assertEqual(command[3], "v1.0\\bwa-mem-tool.cwl")
-            self.assertEqual(command[4], "v1.0\\bwa-mem-job.json")
-        else:
-            self.assertEqual(command[3], "v1.0/bwa-mem-tool.cwl")
-            self.assertEqual(command[4], "v1.0/bwa-mem-job.json")
+def test_unix_relative_path():
+    """Confirm unix style to windows style path corrections."""
+    command = prepare_test_command(
+        tool="cwl-runner",
+        args=[],
+        testargs=None,
+        test={
+            "doc": "General test of command line generation",
+            "output": {"args": ["echo"]},
+            "tool": "v1.0/bwa-mem-tool.cwl",
+            "job": "v1.0/bwa-mem-job.json",
+            "tags": ["required"],
+        },
+        cwd=os.getcwd(),
+    )
+    if os.name == "nt":
+        assert command[3] == "v1.0\\bwa-mem-tool.cwl"
+        assert command[4] == "v1.0\\bwa-mem-job.json"
+    else:
+        assert command[3] == "v1.0/bwa-mem-tool.cwl"
+        assert command[4] == "v1.0/bwa-mem-job.json"
