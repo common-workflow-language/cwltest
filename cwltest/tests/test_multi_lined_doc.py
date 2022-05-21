b29c6fd76906e09b9a202f7c6d312119c077c7a5
Tomoya Tanjo <ttanjo@gmail.com>
2022-05-21 10:19:10 +0000

    Style fix

--- cwltest/tests/test_multi_lined_doc.py
+++ cwltest/tests/test_multi_lined_doc.py
@@ -5,11 +5,13 @@ from pathlib import Path
 from .util import run_with_mock_cwl_runner, get_data
 import defusedxml.ElementTree as ET
 
+
 def test_run():
     args = ["--test", get_data("tests/test-data/multi-lined-doc.yml")]
     error_code, stdout, stderr = run_with_mock_cwl_runner(args)
     assert f"Test [1/2] opt-error: Test with label{n}" in stderr
     assert f"Test [2/2] Test without label{n}" in stderr
+
 
 def test_list():
     args = ["--test", get_data("tests/test-data/multi-lined-doc.yml"), "-l"]
