8456ed754ee096b994fd64da5ead5390874f148f
Tomoya Tanjo <ttanjo@gmail.com>
2022-05-30 14:12:41 +0000

    Add tests

--- cwltest/tests/test_multi_lined_doc.py
+++ cwltest/tests/test_multi_lined_doc.py
@@ -9,6 +9,7 @@ import defusedxml.ElementTree as ET
 def test_run():
     args = ["--test", get_data("tests/test-data/multi-lined-doc.yml")]
     error_code, stdout, stderr = run_with_mock_cwl_runner(args)
+    assert "The `label` field is deprecated. Use `id` field instead." in stderr
     assert f"Test [1/2] opt-error: Test with label{n}" in stderr
     assert f"Test [2/2] Test without label{n}" in stderr
 
