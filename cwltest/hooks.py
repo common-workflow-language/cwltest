2f629c92a7e32b0667a2b6abf5203ae1ddee3ced
Michael R. Crusoe <michael.crusoe@gmail.com>
2023-01-07 16:33:27 +0100

    hook doc: fix formatting

--- cwltest/hooks.py
+++ cwltest/hooks.py
@@ -1,20 +1,23 @@
 """Hooks for pytest-cwl users."""
 
-from typing import Optional
+from typing import Any, Dict, Optional, Tuple
 
 from cwltest import utils
 
 
 def pytest_cwl_execute_test(
     config: utils.CWLTestConfig, processfile: str, jobfile: Optional[str]
-) -> None:
+) -> Tuple[int, Optional[Dict[str, Any]]]:
     """
     Execute CWL test using a Python function instead of a command line runner.
 
-    "processfile" and "jobfile" are both paths.
+    The return value is a tuple.
+     - status code
+        - 0 = success
+        - :py:attr:`cwltest.UNSUPPORTED_FEATURE` for an unsupported feature
+        - and any other number for failure
+     - CWL output object using plain Python objects.
 
-    Returns a tuple:
-    - status code (0=success, cwltest.UNSUPPORTED_FEATURE, for an unsupported feature,
-                   and any other number for failure)
-    - CWL output object using plain Python objects.
+    :param processfile: a path to a CWL document
+    :param jobfile: an optionl path to JSON/YAML input object
     """
